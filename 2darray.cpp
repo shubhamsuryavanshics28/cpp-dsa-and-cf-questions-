9aea029d013bb81588414792d1c657938ee6f4ae
shubham suryavanshi <shubhamvanshi009@gmail.com>
2026-02-15 13:43:26 +0530

    13 feb ka hisab

---
+++ 2darray.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[3][3];
+    for (int i=0;i<3;i++)
+    {
+        for (int j=0;j<3;j++)
+        {
+            cout<<"enter the "<<i<<","<<j<<" element:";
+            cin>>arr[i][j];
+        }
+        
+    }
+    for (int i=0;i<3;i++)
+    {
+        for (int j=0;j<3;j++)
+        {
+            cout<<arr[i][j]<<" ";
+        }cout<<endl;
+        
+    }
+    
+
+    return 0;
+}
