00e2bbec33025c6c3a2115c62472075091ef89a7
shubham suryavanshi <shubhamvanshi009@gmail.com>
2026-02-18 21:08:09 +0530

    binary question

---
+++ binary trees/bfstraversal.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node{
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node(int val){
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+int levels(Node* root){
+    if(root==NULL) return 0;
+    return 1 + max(levels(root->left),levels(root->right));
+}
+void nthlevel(Node* root,int curr,int level){
+    if(root==NULL) return;
+    if(curr==level) cout<<root->val<<" ";
+    nthlevel(root->left,curr+1,level);
+    nthlevel(root->right,curr+1,level);
+}
+void levelorder(Node* root){
+    int n = levels(root);
+    for(int i=1;i<=n;++i){
+        nthlevel(root,1,i);
+        cout<<"\n";
+    }
+}
+int main(){
+    Node* a = new Node(1);
+    Node* b = new Node(2);
+    Node* c = new Node(3);
+    Node* d = new Node(4);
+    Node* e = new Node(5);
+    Node* f = new Node(6);
+    Node* g = new Node(7);
+    Node* h = new Node(8);
+
+    a->left = b;
+    a->right = c;
+    b->left = d;
+    b->right = e;
+    c->left = f;
+    c->right = g;
+    //g->left = h;
+
+    //nthlevel(a,1,3);
+    levelorder(a);
+
+    return 0;
+}
