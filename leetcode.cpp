e03c6e5a1c4d09d3f655507ba53e819a4724051f
shubham suryavanshi <shubhamvanshi009@gmail.com>
2026-02-25 13:58:28 +0530

    question upated

--- leetcode.cpp
+++ leetcode.cpp
@@ -2,32 +2,14 @@
 using namespace std;
 
 int main(){
-    vector<int> nums={-1,4,-1};
-    int n=nums.size();
-    vector<int> result;
-    int lo=0;
-    int hi=lo;
-    while(lo<n){
-        if(nums[lo]>0){
-            hi += nums[lo];
-            int a = hi%n;
-            result.push_back(nums[a]);
-            lo++;
-            hi = lo;
-        }else if(nums[lo]<0){
-            hi += abs(nums[lo]);
-            int a = hi%n;
-            result.push_back(nums[hi]);
-            lo++;
-            hi = lo;
-        }else{
-            result.push_back(nums[lo]);
-            lo++;
-            hi=lo;
-        }
+    vector<vector<int>> triangle={{2},{3,4},{6,5,7},{4,1,8,3}};
+    int sum = 0;
+    for(int i=0;i<triangle.size();++i){
+        vector<int> v = triangle[i];
+        int a = *min_element(v.begin(),v.end());
+        sum += a;
     }
-    for(int i=0;i<result.size();++i){
-        cout<<result[i]<<" ";
-    }
+    cout<<sum<<endl;
+
     return 0;
 }
