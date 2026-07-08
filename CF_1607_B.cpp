1882acad9a395e2fa382c30805ab0bececfd0f88
shubham.suryavanshi.cs28@iilm.edu <shubham.suryavanshi.cs28@iilm.edu>
2026-07-08 21:39:22 +0530

    cf 1607B soln

---
+++ CF_1607_B.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define MOD 1000000007
+
+#define returnYES {std::cout << "YES" << std::endl; return;}
+#define returnNO  {std::cout << "NO"  << std::endl; return;}
+template <typename T>
+void print(const vector<T> &v)
+{
+    for (auto &x : v)
+        cout << x << " ";
+    cout << "\n";
+}
+
+template <typename T>
+void input(vector<T> &v, int n)
+{
+    v.resize(n);
+    for (auto &x : v)
+        cin >> x;
+}
+
+void solve()
+{
+    long long x, n;
+    cin >> x >> n;
+
+    long long rem = n % 4;
+    long long count = n - rem + 1;
+    while(rem--)
+    {
+        if(x % 2 == 0) x -= count;
+        else x += count;
+        count++;
+    }
+    cout<<x<<endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
