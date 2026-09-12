4bec33f2f6db94d9e900749ed91abf2dacecd2c9
shubham.suryavanshi.cs28@iilm.edu <shubham.suryavanshi.cs28@iilm.edu>
2026-09-12 16:22:16 +0530

    dice combinations by dp

--- dp/dice_combinations.cpp
+++ dp/dice_combinations.cpp
@@ -3,14 +3,27 @@ using namespace std;
 
 const int MOD = 1e9+7;
 
-int helper(int n,vector<int>& dp){
-    if(n==0) return 1;
-    if(dp[n]!=-1) return dp[n];
-    dp[n] = 0;
-    for(int i=1;i<=6;++i){
-        if(n-i>=0){
-            dp[n] = (1LL * dp[n] + helper(n-i,dp)) % MOD;
+// int helper(int n,vector<int>& dp){
+//     if(n==0) return 1;
+//     if(dp[n]!=-1) return dp[n];
+//     dp[n] = 0;
+//     for(int i=1;i<=6;++i){
+//         if(n-i>=0){
+//             dp[n] = (1LL * dp[n] + helper(n-i,dp)) % MOD;
+//         }
+//     }
+//     return dp[n];
+// }
+int fbu(int n,vector<int>& dp){
+    dp[0] = 1;
+
+    for(long long k=1;k<=n;k++){
+        long long sum = 0;
+        for(long long i=1;i<=n;++i){
+            if(k-i<0) break;
+            sum = (sum%MOD + dp[k-i]%MOD)%MOD;
         }
+        dp[k] = sum;
     }
     return dp[n];
 }
@@ -18,7 +31,8 @@ int helper(int n,vector<int>& dp){
 int main() {
     int n; cin>>n;
     vector<int> dp(1000006,-1);
-    int ans = helper(n,dp);
+    //int ans = helper(n,dp);
+    int ans = fbu(n,dp);
 
     cout<<ans<<endl;
 
